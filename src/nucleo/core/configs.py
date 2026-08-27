24115839ffb52b5bd45f6858c3c14edd2a6ed5b0
Nicolas_PelletBossot <nicolas.pellet.tlse@gmail.com>
2026-08-27 15:21:51 +0200

    Y2026 M08 D27

--- src/nucleo/core/configs.py
+++ src/nucleo/core/configs.py
@@ -166,8 +166,8 @@ def choose_configuration(config: str) -> dict:
             "s": np.array([35], dtype=int)
         }, 
         "probas": {
-            "mu": np.array([150], dtype=int),
-            "theta": np.array([25, 50, 100], dtype=int),
+            "mu": np.array([150, 500], dtype=int),
+            "theta": np.array([50, 100, 300], dtype=int),
             "alphao": np.array([PROBAS["alphao"]], dtype=float),
             "alphaf": np.array([PROBAS["alphaf"]], dtype=float),
             "beta": np.array([PROBAS["beta"]], dtype=float),
